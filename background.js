39d97cf57b0d97d25a28432a6a331c62f136ddf3
Hunter Cote <hcote12@gmail.com>
2019-05-29 20:46:22 -0400

    highlight and unhighlight functionality working

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
 // extension injects JS ONLY when icon is clicked
-let status = false;
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    if (status === false) {
-        status = true;
-        // chrome.browserAction.setIcon({path: "images/icon-active.png"});
+        chrome.tabs.insertCSS(tab.id, {file: "highlighter.css"});
         chrome.tabs.executeScript(tab.id, {file: "highlighter.js"});
-    } else {
-        status = false;
-    }
 });
--- highlighter.js
+++ highlighter.js
@@ -1,35 +1,23 @@
-function highlight() {
-        // text is an object of the element(s) you highlight
-        var text = window.getSelection();
-        console.log(text);
-        
-        // <span>...</span>
-        let span = document.createElement('SPAN');
-        console.log(span);
-        
-        // need to use the toString() to turn object into a string
-        span.textContent = text.toString();
-        console.log(span.textContent);
-        
-        // range now represents the range that was highlighted
-        let range = text.getRangeAt(0);
-        console.log(range);
-        
-        // delete contents of range
-        range.deleteContents();
 
-        // insert contents of highlighted values into highlighted range
-        range.insertNode(span);
+// function highlight() {
+//     let span = document.createElement('span');
+//     window.getSelection().getRangeAt(0).surroundContents(span);
+//     span.className = "highlighted";
+// };
 
-        // style it with a background color
-        span.style.backgroundColor = "#C7FFD8";
-};
-
-console.log('highlighter turned on');
 document.getElementsByTagName("body")[0].onmouseup = highlight();
-if (document.getElementsByTagName("body")[0].onmouseup) {
-    console.log(true);
-    
-}
-// window.addEventListener('mouseup', highlight());
 
+function highlight() {
+    document.designMode = "on";
+    var text = window.getSelection();
+    var range = window.getSelection().getRangeAt(0);
+    text.removeAllRanges();
+    text.addRange(range);
+    if (text.anchorNode.parentElement.style.backgroundColor == 'rgb(199, 255, 216)') {
+        text.anchorNode.parentElement.style.backgroundColor = 'transparent';        
+    } else {
+        document.execCommand("HiliteColor", false, '#C7FFD8');
+    }
+    
+    document.designMode = "off";
+};
