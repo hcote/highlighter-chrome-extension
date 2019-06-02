68702730a1430e7e9582030a983b5ee5d282aa93
Hunter Cote <hcote12@gmail.com>
2019-06-02 12:11:42 -0400

    added keyboard shortcut for highlighter activation

--- background.js
+++ background.js
@@ -1,14 +1,22 @@
 chrome.commands.onCommand.addListener((command) => {
+    if (command === "highlight_text") {
+        chrome.tabs.query({'active': true}, (tab) => {
+            chrome.tabs.insertCSS(tab.id, {file: "highlighter.css"});
+            chrome.tabs.executeScript(tab.id, {file: "injection_script.js"});
+        })
+    };
     if (command === "clear_storage") {
-    chrome.tabs.query({'active': true}, (tab) => {
-            chrome.tabs.executeScript(tab.id, {file: "clear_storage.js"});  
-            alert('made it')
+        chrome.tabs.query({'active': true}, (tab) => {
+            chrome.tabs.executeScript(tab.id, {file: "clear_storage.js"});
         })
     }
 });
 
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.insertCSS(tab.id, {file: "highlighter.css"});
-    chrome.tabs.executeScript(tab.id, {file: "injection_script.js"});
-});
+// chrome.commands.onCommand.addListener((command) => {
+    
+// });
+
+
+// chrome.browserAction.onClicked.addListener((tab) => {
+// });
 
