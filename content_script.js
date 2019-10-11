f7adca1fdb1bc59ae8af9ae8155bf166159c2cfb
Hunter Cote <huntercote2@gmail.com>
2019-10-10 17:00:07 -0700

    got tooltip to show up next to highlight

--- content_script.js
+++ content_script.js
@@ -2,81 +2,93 @@ var url = window.location.href.toString();
 var DOM = document.body;
 var highlights, note;
 
+insertDiv = () => {};
+
 function searchForHighlights() {
-    chrome.storage.local.get('highlights', (results) => {
-        if (objDoesNotExist(results)) {
-            createHighlightObj();
-        } else {
-            if (doHighlightsForThisURLExist(results)) {
-                return;
-            } else {
-                applyHighlights(results.highlights[url]);
-                console.log(results);
-                addPromptToTargets();
-            }
-        }
-    });
+  insertDiv();
+  chrome.storage.local.get("highlights", results => {
+    if (objDoesNotExist(results)) {
+      createHighlightObj();
+    } else {
+      if (doHighlightsForThisURLExist(results)) {
+        return;
+      } else {
+        applyHighlights(results.highlights[url]);
+        console.log(results);
+        addPromptToTargets();
+      }
+    }
+  });
 }
 
 function objDoesNotExist(results) {
-    if (results === 'undefined' || Object.entries(results).length === 0 && results.constructor === Object) {
-        return true;
-    }
+  if (
+    results === "undefined" ||
+    (Object.entries(results).length === 0 && results.constructor === Object)
+  ) {
+    return true;
+  }
 }
 
 function createHighlightObj() {
-    highlights = {
-        color: 'rgb(207, 255, 223)'
-    };
-    chrome.storage.local.set({highlights}, () => {        
-    });
-    return;
+  highlights = {
+    color: "rgb(207, 255, 223)"
+  };
+  chrome.storage.local.set({ highlights }, () => {});
+  return;
 }
 
 function doHighlightsForThisURLExist(results) {
-    if (!results.highlights[url]) {
-        return true;
-    }
+  if (!results.highlights[url]) {
+    return true;
+  }
 }
 
 function applyHighlights(pageHighlights) {
-    console.log('Highlights Found For This URL');
-    for (key in pageHighlights) {
-        var nodeList = document.body.querySelectorAll(pageHighlights[key][0]); // NodeList(4) [queryselector, ...]
-        for (let i = 0; i < nodeList.length; i++) {            
-            if (pageHighlights[key][1] === nodeList[i].innerText.indexOf(key) || pageHighlights[key][2] === nodeList[i].innerText.indexOf(key)) {
-                grabNoteIfExists(pageHighlights);
-                nodeList[i].innerHTML = nodeList[i].innerHTML.replace(key, `<span style="background-color: rgb(199, 255, 216);" class="el" title="${note}">`+key+'</span>');
-            }
-        }
+  console.log("Highlights Found For This URL");
+  for (key in pageHighlights) {
+    var nodeList = document.body.querySelectorAll(pageHighlights[key][0]); // NodeList(4) [queryselector, ...]
+    for (let i = 0; i < nodeList.length; i++) {
+      if (
+        pageHighlights[key][1] === nodeList[i].innerText.indexOf(key) ||
+        pageHighlights[key][2] === nodeList[i].innerText.indexOf(key)
+      ) {
+        grabNoteIfExists(pageHighlights);
+        nodeList[i].innerHTML = nodeList[i].innerHTML.replace(
+          key,
+          `<span style="background-color: rgb(199, 255, 216);" class="el" title="${note}">` +
+            key +
+            "</span>"
+        );
+      }
     }
+  }
 }
 
 function grabNoteIfExists(phls) {
-    if (phls[key][3] != undefined) {
-        note = phls[key][3].toString();
-    } else {
-        note = '';
-    }
+  if (phls[key][3] != undefined) {
+    note = phls[key][3].toString();
+  } else {
+    note = "";
+  }
 }
 
 searchForHighlights();
 
 function addPromptToTargets() {
-    var nodes = document.getElementsByClassName("el");
-    for (let i = 0; i < nodes.length; i++) {
-        nodes[i].ondblclick = () => {
-            note = prompt('Add a comment for this highlight: ', nodes[i].title);
-            if (note != null) {
-                chrome.storage.local.get('highlights', (results) => {
-                    highlights = results.highlights;
-                    highlight = highlights[url][nodes[i].innerHTML];
-                    highlight[3] = note;
-                    nodes[i].title = note;
-                    chrome.storage.local.set({highlights}, () => {
-                    });
-                });
-            }
-        };
-    }
+  var nodes = document.getElementsByClassName("el");
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].ondblclick = () => {
+      note = prompt("Add a comment for this highlight: ", nodes[i].title);
+      if (note != null) {
+        chrome.storage.local.get("highlights", results => {
+          highlights = results.highlights;
+          highlight = highlights[url][nodes[i].innerHTML];
+          highlight[3] = note;
+          nodes[i].title = note;
+          chrome.storage.local.set({ highlights }, () => {});
+        });
+      }
+    };
+  }
 }
--- injection_script.js
+++ injection_script.js
@@ -5,73 +5,105 @@ var url = window.location.href.toString();
 var text, range, hTag, savedText, key, querySelector, hex;
 
 function getBlockElementForQS() {
-    if (hTag.tagName == "A" || hTag.tagName == "CODE" || hTag.tagName == "EM" || hTag.tagName == "STRONG" ||  hTag.tagName == "SPAN") {
-        hTag = hTag.parentElement;
-        getBlockElementForQS();
-    }
+  if (
+    hTag.tagName == "A" ||
+    hTag.tagName == "CODE" ||
+    hTag.tagName == "EM" ||
+    hTag.tagName == "STRONG" ||
+    hTag.tagName == "SPAN"
+  ) {
+    hTag = hTag.parentElement;
+    getBlockElementForQS();
+  }
 }
 
 function grabSelectedText() {
-    text = window.getSelection();
-    range = window.getSelection().getRangeAt(0);
-    hTag = text.anchorNode.parentElement;
-    savedText = text;
-    text.removeAllRanges();
-    text.addRange(range);
-};
+  text = window.getSelection();
+  range = window.getSelection().getRangeAt(0);
+  hTag = text.anchorNode.parentElement;
+  savedText = text;
+  text.removeAllRanges();
+  text.addRange(range);
+}
 
 function assignQuerySelector() {
-    if (!hTag.className) {
-        querySelector = hTag.tagName.toLowerCase();
-    } else {
-        // need to replace className string spaces with dots 
-        // (that's how querySelectorAll grabs elements with multiple classes)
-        querySelector = hTag.tagName.toLowerCase() + "." + hTag.className.split(' ').join('.');
-    }
-};
+  if (!hTag.className) {
+    querySelector = hTag.tagName.toLowerCase();
+  } else {
+    // need to replace className string spaces with dots
+    // (that's how querySelectorAll grabs elements with multiple classes)
+    querySelector =
+      hTag.tagName.toLowerCase() + "." + hTag.className.split(" ").join(".");
+  }
+}
 
 function saveHighlight() {
-    chrome.storage.local.get('highlights', (results) => { 
-        highlights = results.highlights
-        // if no stored highlights for URL, initialize empty obj for text: qS value pairs
-        if (!results.highlights[url]) {
-            highlights[url] = {};            
-        }
-        assignQuerySelector();    
-        highlights[url][savedText.anchorNode.parentElement.innerHTML] = [querySelector, hTag.innerText.indexOf(savedText.toString().trim()), hTag.innerHTML.indexOf(savedText.toString().trim())];
-        chrome.storage.local.set({highlights}, () => {
-        });
-    });
-};
+  chrome.storage.local.get("highlights", results => {
+    highlights = results.highlights;
+    // if no stored highlights for URL, initialize empty obj for text: qS value pairs
+    if (!results.highlights[url]) {
+      highlights[url] = {};
+    }
+    assignQuerySelector();
+    highlights[url][savedText.anchorNode.parentElement.innerHTML] = [
+      querySelector,
+      hTag.innerText.indexOf(savedText.toString().trim()),
+      hTag.innerHTML.indexOf(savedText.toString().trim())
+    ];
+    chrome.storage.local.set({ highlights }, () => {});
+  });
+}
 
 function removeHighlight() {
-    savedText.anchorNode.parentElement.style.backgroundColor = 'transparent';
-        chrome.storage.local.get('highlights', (results) => {            
-        highlights = results.highlights;
-        delete highlights[url][savedText.anchorNode.parentElement.innerHTML]
-        chrome.storage.local.set({highlights}, () => { 
-        });
-    });
-};
+  savedText.anchorNode.parentElement.style.backgroundColor = "transparent";
+  chrome.storage.local.get("highlights", results => {
+    highlights = results.highlights;
+    delete highlights[url][savedText.anchorNode.parentElement.innerHTML];
+    chrome.storage.local.set({ highlights }, () => {});
+  });
+}
 
 function executeHighlight() {
-    document.execCommand("HiliteColor", false, '#CFFFDF');
+  document.execCommand("HiliteColor", false, "#CFFFDF");
 }
 
 function addClassToSelectedText() {
-    savedText.anchorNode.parentElement.className = "el";
-};
+  savedText.anchorNode.parentElement.className = "el";
+}
 
 function activateExtension() {
-    document.designMode = "on";
-    grabSelectedText();
-    getBlockElementForQS();
-    if (savedText.anchorNode.parentElement.style.backgroundColor == 'rgb(199, 255, 216)') {
-        removeHighlight();
-    } else {
-        executeHighlight();
-        addClassToSelectedText();
-        saveHighlight();
-    }
-    document.designMode = "off";
-};
+  if (window.getSelection().getRangeAt(0)) {
+    var sel = window.getSelection();
+    var sel_text = sel.toString();
+    var span = document.createElement("SPAN");
+    span.classList.add("toHL");
+    span.innerText = sel_text;
+    var range = window.getSelection().getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(span);
+
+    var tt = document.createElement("DIV");
+    tt.innerHTML = "Highlight";
+    tt.style.textAlign = "center";
+    tt.style.borderRadius = "8px";
+    tt.style.padding = "5px";
+    tt.style.fontSize = "14px";
+    tt.style.backgroundColor = "rgba(38, 39, 41)";
+    tt.style.color = "aliceblue";
+    tt.style.display = "inline";
+    tt.style.position = "absolute";
+    tt.id = "tt";
+    document.getElementsByClassName("toHL")[0].appendChild(tt);
+  }
+  // document.designMode = "on";
+  // grabSelectedText();
+  // getBlockElementForQS();
+  // if (savedText.anchorNode.parentElement.style.backgroundColor == 'rgb(199, 255, 216)') {
+  //     removeHighlight();
+  // } else {
+  //     executeHighlight();
+  //     addClassToSelectedText();
+  //     saveHighlight();
+  // }
+  // document.designMode = "off";
+}
